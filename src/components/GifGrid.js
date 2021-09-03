12f124f3b9a4c05fe397a6fff80268387196c84f
Carlos <crdgom@gmail.com>
2021-09-03 09:30:17 -0500

    Added custom hooks and loading flag

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,19 +1,16 @@
-import React, { useState, useEffect} from 'react'
-import { getGifs } from '../helpers/getGifs';
-import { GifGridItem } from './GifGridItem';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+import {GifGridItem} from './GifGridItem'
+
 
 export const GifGrid = ({category}) => {
 
-    const [images, setImages] = useState(['']);
+    const {data:images, loading} = useFetchGifs( category );
     
-    useEffect(() => {
-        getGifs(category)
-            .then(setImages)
-    }, [category])
-  
     return (
         <>
         <h3>{category}</h3>
+        {loading && <p>Loading...</p>}
         <div className="card-grid">
             {
                 images.map((img) => (
